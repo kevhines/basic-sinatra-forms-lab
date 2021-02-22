681b384fdc955c506559e18674ba7fd3784e07bd
Kevin <kevhines@gmail.com>
2021-02-22 15:45:29 -0500

    commit tree 578954cbbb9ff5a1b007a833a7fbe9d74f260f8f

--- app.rb
+++ app.rb
@@ -2,5 +2,15 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get "/" do
+        "<a href='/newteam'>New Team</a>"
+    end
+    get "/newteam" do
+        erb :newteam
+    end
+    post "/team" do
+        @team = params
+        erb :team
+    end
 
 end
